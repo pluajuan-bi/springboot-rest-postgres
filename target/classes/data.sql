59c39a067e2f267ed8a38e9758ec1e4211cbc041
pluaj <plua.juan@fisagrp>
2025-11-07 07:27:50 -0500

    target

---
+++ target/classes/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO usuario ( nombre, email) VALUES ( 'Juan', 'juan@example.com');
+INSERT INTO usuario ( nombre, email) VALUES ( 'Maria', 'maria@example.com');
+INSERT INTO usuario ( nombre, email) VALUES ( 'Pedro', 'pedro@example.com');
